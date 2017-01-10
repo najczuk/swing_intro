fe081e979f64ea4b8b7808024c9d43e7228f11c1
Adrian Najczuk <Adrian Najczuk>
2017-01-10 21:02:59 +0100

    working calculator

--- src/pl/sda/calculator/RPNUtil.java
+++ src/pl/sda/calculator/RPNUtil.java
@@ -6,7 +6,45 @@ import java.util.Stack;
 public class RPNUtil {
 	
 	public static void main(String[] args){
-		System.out.println(convertCalculationToRPN("( 2 + 3 * 2 )"));
+		String calculation = "( ( 2 + 3 ) * 2 )";
+		System.out.println(convertCalculationToRPN(calculation));
+		String rpnCalculation = convertCalculationToRPN(calculation);
+		System.out.println(calculateRPN(rpnCalculation));
+	}
+	
+	public static Double calculateRPN(String rpnCalculation){
+		Scanner scanner = new Scanner(rpnCalculation);
+		Stack<Double> stack = new Stack<>();
+		
+		String token;
+		Double v1,v2;
+		
+		while(scanner.hasNext()){
+			token = scanner.next();
+			if(!isOperator(token)){
+				stack.push(Double.valueOf(token));
+			} else {
+				v2 = stack.pop();
+				v1 = stack.pop();
+				stack.push(calculate(v1, v2, token));
+				
+			}
+		}
+		
+		
+		return stack.pop(); //TODO
+		
+	}
+	
+	private static Double calculate(Double v1, Double v2, String operator){
+		switch(operator){
+			case "+" : return v1 + v2;
+			case "-" : return v1 - v2;
+			case "/" : return v1 / v2;
+			case "*" : return v1 * v2;
+			case "^" : return Math.pow(v1, v2);
+		}
+		return null;
 	}
 
 	public static String convertCalculationToRPN(String calculation) {
