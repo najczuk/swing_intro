a7477ad2eec7218524e6ae70adf3d77ef95ff5c8
Adrian Najczuk <najczuk@gmail.com>
2017-01-11 01:04:34 +0100

    poprawka RPNUtil przy liczeniu RPN

--- src/pl/sda/calculator/RPNUtil.java
+++ src/pl/sda/calculator/RPNUtil.java
@@ -4,108 +4,123 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class RPNUtil {
-	
-	public static void main(String[] args){
-		String calculation = "( ( 2 + 3 ) * 2 )";
-		System.out.println(convertCalculationToRPN(calculation));
-		String rpnCalculation = convertCalculationToRPN(calculation);
-		System.out.println(calculateRPN(rpnCalculation));
-	}
-	
-	public static Double calculateRPN(String rpnCalculation){
-		Scanner scanner = new Scanner(rpnCalculation);
-		Stack<Double> stack = new Stack<>();
-		
-		String token;
-		Double v1,v2;
-		
-		while(scanner.hasNext()){
-			token = scanner.next();
-			if(!isOperator(token)){
-				stack.push(Double.valueOf(token));
-			} else {
-				v2 = stack.pop();
-				v1 = stack.pop();
-				stack.push(calculate(v1, v2, token));
-				
-			}
-		}
-		
-		
-		return stack.pop(); //TODO
-		
-	}
-	
-	private static Double calculate(Double v1, Double v2, String operator){
-		switch(operator){
-			case "+" : return v1 + v2;
-			case "-" : return v1 - v2;
-			case "/" : return v1 / v2;
-			case "*" : return v1 * v2;
-			case "^" : return Math.pow(v1, v2);
-		}
-		return null;
-	}
 
-	public static String convertCalculationToRPN(String calculation) {
-		String rpmCalculation = "";
+    public static void main(String[] args) {
+        String calculation = "( 2 + 3 ) * 2";
+        System.out.println(convertCalculationToRPN(calculation));
+        String rpnCalculation = convertCalculationToRPN(calculation);
+        System.out.println(calculateRPN(rpnCalculation));
+    }
+
+    public static Double calculateRPN(String rpnCalculation) {
+        Scanner scanner = new Scanner(rpnCalculation);
+        Stack<Double> stack = new Stack<>();
+
+        String token;
+        Double v1, v2;
+
+        while (scanner.hasNext()) {
+            token = scanner.next();
+            if (!isOperator(token)) {
+                stack.push(Double.valueOf(token));
+            } else {
+                v2 = stack.pop();
+                v1 = stack.pop();
+                stack.push(calculate(v1, v2, token));
+
+            }
+        }
+
+
+        return stack.pop(); //TODO
+
+    }
+
+    private static Double calculate(Double v1, Double v2, String operator) {
+        switch (operator) {
+            case "+":
+                return v1 + v2;
+            case "-":
+                return v1 - v2;
+            case "/":
+                return v1 / v2;
+            case "*":
+                return v1 * v2;
+            case "^":
+                return Math.pow(v1, v2);
+        }
+        return null;
+    }
+
+    public static String convertCalculationToRPN(String calculation) {
+        String rpmCalculation = "";
         Stack<String> stack = new Stack<>();
 
-        Scanner tokenizer = new Scanner(calculation);
+        Scanner scanner = new Scanner(calculation);
 
         String token;
         int tokenPriority;
-        while (tokenizer.hasNext()) {
-            token = tokenizer.next();
+        while (scanner.hasNext()) {
+            token = scanner.next();
             tokenPriority = getOperatorPriority(token);
+
             if (token.equals("(")) {
                 stack.push(token);
-                continue;
             } else if (token.equals(")")) {
                 while (!stack.peek().equals("(")) {
-                    rpmCalculation = rpmCalculation + stack.pop() + " ";
+                    rpmCalculation += stack.pop() + " ";
                 }
-                stack.pop(); //removes ( from stack
-                continue;
+                stack.pop();
             } else if (isOperator(token)) {
                 while (!stack.empty()) {
-                    if (tokenPriority == 3 || (tokenPriority > getOperatorPriority(stack.peek()))) {
+                    if (tokenPriority == 3 || tokenPriority > getOperatorPriority(stack.peek())) {
                         stack.push(token);
                         break;
                     } else {
-                        rpmCalculation = rpmCalculation + stack.pop() + " ";
+                        rpmCalculation += stack.pop() + " ";
+                    }
+                    if (stack.empty()) {
+                        stack.push(token);
                     }
                 }
+                if (stack.empty()) {
+                    stack.push(token);
+                }
             } else {
-                rpmCalculation = rpmCalculation + token + " ";
+                rpmCalculation += token + " ";
             }
 
+
+        }
+
+        while (!stack.empty()) {
+            rpmCalculation += stack.pop() + " ";
         }
         return rpmCalculation;
 
-	}
-	
-	private static boolean isOperator(String token){
-		return "()+-*/^".contains(token);
-	}
+    }
 
-	private static int getOperatorPriority(String operator) {
-		switch (operator) {
-		case "(":
-			return 0;
-		case "+":
-			return 1;
-		case "-":
-			return 1;
-		case "*":
-			return 2;
-		case "/":
-			return 2;
-		case "^":
-			return 3;
-		default:
-			return -1;
-		}
-	}
+    private static boolean isOperator(String token) {
+        return "+-*/^".contains(token);
+    }
+
+    private static int getOperatorPriority(String operator) {
+        switch (operator) {
+            case "(":
+                return 0;
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+            case "*":
+                return 2;
+            case "/":
+                return 2;
+            case "^":
+                return 3;
+            default:
+                return -1;
+        }
+    }
 
 }
